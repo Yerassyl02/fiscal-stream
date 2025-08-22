8df474597bc28b467a5ee2016740092e2f573468
Ерасыл Турарбек <yerassyl@MacBook-Air-Erasyl.local>
2025-08-22 20:53:48 +0500

    clean json api

---
+++ main.py
@@ -0,0 +1,87 @@
+from sse_starlette.sse import EventSourceResponse
+from datetime import datetime
+from fastapi import FastAPI
+from faker import Faker
+import asyncio
+import random
+import json
+
+app = FastAPI()
+fake = Faker("ru_RU")
+
+with open("products.json", "r", encoding="utf-8") as f:
+    products_data = json.load(f)["products"]
+
+def generate_dirty_fiscal_line():
+    company_name = fake.company()
+    tax_payer_iin = fake.random_number(digits=12)
+    kkt_factor_number = fake.bothify('?#########')
+    kkt_reg_number = fake.random_number(digits=12)
+    receipt_number = fake.random_int(min=1, max=999999)
+    date_time = datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
+
+    # orders
+    items = []
+    for _ in range(random.randint(1, 15)):
+        category = random.choice(list(products_data.keys()))
+        product = random.choice(products_data[category]) 
+        price = round(random.uniform(100, 500000), 2)
+        quantity = random.randint(1, 5)
+        total =round(price * quantity, 2)
+        vat_rate = random.choice(['12%', '0%'])
+        vat_sum = round(total * (0.12 if vat_rate == '12%' else 0), 2)
+
+        items.append({
+            'Category': category,
+            'Name': product,
+            'UnitPrice': price,
+            'Quantity': quantity,
+            'Total': total,
+            'VAT': {
+                'Rate': vat_rate,
+                'Sum': vat_sum
+            }
+        })
+
+    total_sum = sum(item['Total'] for item in items)
+
+    fiscal_sign  = fake.random_number(digits=10)
+
+    ofd_name = random.choice(["АО Казахтелеком", "АО Транстелеком"])
+    ofd_site = 'https://ofd.telecom.kz' if ofd_name == 'АО Казахтелеком' else 'https://ofd.ttc.kz'
+    ofd = {
+        'Name': ofd_name,
+        'Site': ofd_site
+    }
+
+    address = f"{fake.city()}, {fake.street_address()}"
+    qr_code = fake.lexify(text="????????????????????????")
+
+    return (
+        {
+            'CompanyName': company_name,
+            'TaxPayerIIN': tax_payer_iin,
+            'KKTFactorNumber (ЗНМ)': kkt_factor_number,
+            'KKTRegistrationNumber (РНМ)': kkt_reg_number,
+            'ReceiptNumber': receipt_number,
+            'DateTime': date_time,
+            'Items': items,
+            'FiscalSign (ФП)': fiscal_sign,
+            'OFD': ofd,
+            'Address': address,
+            'QRCode': qr_code
+        }
+    )
+
+@app.get("/stream")
+async def stream():
+    async def event_generator():
+        while True:
+            await asyncio.sleep(random.uniform(0.5, 2.5))  # рандомная задержка
+            line = generate_dirty_fiscal_line()
+            yield {
+                "event": "dirty_fiscal_log",
+                "data": line
+            }
+
+    return EventSourceResponse(event_generator())
